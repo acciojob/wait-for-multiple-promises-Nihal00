e873d336cfe9f12e561ca1f79ed9541e7388f840
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-25 14:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const output document.getElementById('output');
+
+const promises = [];
+
+const prom1 = new Promise((resolve) => setTimeout(resolve, 1000, "Promise 1"));
+promises.push(prom1);
+
+const prom2 = new Promise((resolve) => setTimeout(resolve, 2000, "Promise 2"));
+promises.push(prom2);
+
+const prom3 = new Promise((resolve) => setTimeout(resolve, 3000, "Promise 3"));
+promises.push(prom3);
+
+Promise.all(promises).then((value) => {
+	document.getElementById('')
+});
