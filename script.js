e95b3aee150ac253ff9e2b401fcf93264587d1b8
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-28 11:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,96 @@
-//your JS code here. If required.
-const output = document.getElementById('output');
-// const table = document.querySelector('table');
+// //your JS code here. If required.
 
+const output = document.getElementById('output');
 const row = output.insertRow(-1);
 const col1 = row.insertCell(0);
 const col2 = row.insertCell(1);
 col1.innerText = "Loading...";
 col2.innerText = "Loading...";
 
-const promises = [
-	new Promise (resolve => {
-		setTimeout(() => {
-			resolve();
-			return "Promise 1";
-		}, Math.random() * 3000);
-	}),
+const promises = [];
 
-	new Promise (resolve => {
-		setTimeout(() => {
-			resolve();
-		}, Math.random() * 3000);
-	}),
+for(let i=0; i<3; i++){
+	promises.push(new Promise(function(resolve, reject) {
 
-	new Promise (resolve => {
-		setTimeout(() => {
-			resolve()
-		}, Math.random() * 3000);
-	})
-];
+		let randomNubmer = Math.floor(Math.random() * 3) + 1;
 
-const timeArray = [];
-Promise.all(promises).then((result) => {
+		setTimeout(function () {
+			resolve(randomNubmer);
+		}, randomNubmer * 1000);
+	}));
+}
 
+Promise.all(promises).then(function (results) {
+	let totalTime = results.reduce(function (a, b) {
+		return a+b;
+	}, 0);
+	
 	row.remove();
+	let i = 0;
+	for(i=0; i < results.length; i++) {
+		const row = output.insertRow(-1);
+		const col1 = row.insertCell(0);
+		const col2 = row.insertCell(1);
+		col1.innerText = "Promise " + (i+1);
+		col2.innerText = results[i];
+	}
 
-	const start = performance.now();
-	const tableRow = output.insertRow(-1);
-	const tableCel1 = tableRow.insertCell(0);
-	const tableCel2 = tableRow.insertCell(1);
-	const end = performance.now();
-	const time = end - start;
-	timeArray.push(time);
-	tableCel1.innerText = value;
+	if(i === 3){
+		const row = output.insertRow(-1);
+		const col1 = row.insertCell(0);
+		const col2 = row.insertCell(1);
+		col1.innerText = "Total";
+		col2.innerText = totalTime;
+	}
 });
+
+
+
+
+// const output = document.getElementById('output');
+// // const table = document.querySelector('table');
+
+// const row = output.insertRow(-1);
+// const col1 = row.insertCell(0);
+// const col2 = row.insertCell(1);
+// col1.innerText = "Loading...";
+// col2.innerText = "Loading...";
+
+// const promises = [
+// 	new Promise (resolve => {
+// 		setTimeout(() => {
+// 			resolve();
+// 			return "Promise 1";
+// 		}, Math.random() * 3000);
+// 	}),
+
+// 	new Promise (resolve => {
+// 		setTimeout(() => {
+// 			resolve();
+// 		}, Math.random() * 3000);
+// 	}),
+
+// 	new Promise (resolve => {
+// 		setTimeout(() => {
+// 			resolve()
+// 		}, Math.random() * 3000);
+// 	})
+// ];
+
+// const timeArray = [];
+// Promise.all(promises).then((result) => {
+
+// 	row.remove();
+
+// 	const start = performance.now();
+// 	const tableRow = output.insertRow(-1);
+// 	const tableCel1 = tableRow.insertCell(0);
+// 	const tableCel2 = tableRow.insertCell(1);
+// 	const end = performance.now();
+// 	const time = end - start;
+// 	timeArray.push(time);
+// 	tableCel1.innerText = value;
+// });
 
 
 
